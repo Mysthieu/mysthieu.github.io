e4dc65db7d12a71fff2835881bb5b7bc35462785
Mysthieu <120282334+Mysthieu@users.noreply.github.com>
2023-02-05 14:07:09 +0100

    test

--- js/script2.js
+++ js/script2.js
@@ -1,3 +1,4 @@
+alert(document.cookie.substring(11))
 var instruments = toString(parseInt(document.cookie.substring(11))+1);
 alert(instruments)
 document.cookie="instruments=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
