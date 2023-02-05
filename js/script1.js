87bfb1abf54e55055aeaf2a2aa2bf08ea33a30da
Mysthieu <120282334+Mysthieu@users.noreply.github.com>
2023-02-05 13:30:50 +0100

    test

--- js/script1.js
+++ js/script1.js
@@ -1,4 +1,4 @@
 var pseudo = prompt("Entrez votre pseudo");
 document.getElementById('pseudo').innerHTML = pseudo;
 var instruments = "0"
-document.cookie="pseudo="+pseudo+";path=/;";
+document.cookie="instruments="+pseudo+";path=/;";
