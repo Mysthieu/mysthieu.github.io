fa38ffaccb936a4416fd15a89e9eb4286be8e4f2
Mysthieu <120282334+Mysthieu@users.noreply.github.com>
2023-02-05 13:38:17 +0100

    test

--- js/script1.js
+++ js/script1.js
@@ -1,4 +1,4 @@
 var pseudo = prompt("Entrez votre pseudo");
 document.getElementById('pseudo').innerHTML = pseudo;
 var instruments = "0"
-document.cookie="instruments="+pseudo+";path=/;";
+document.cookie="instruments="+intruments+";path=/;";
--- js/script2.js
+++ js/script2.js
@@ -1,4 +1,5 @@
 var instruments = toString(parseInt(document.cookie.substring(11))+1)
 document.cookie="instruments=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 document.cookie="instruments="+instruments+";path=/"
+alert(document.cookie)
 
